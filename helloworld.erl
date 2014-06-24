f28ae09ce862f78b4e417fbb1c9558aff9472d05
Indexer <yemonkyaw007@gmail.com>
2014-06-24 05:05:02 -0400

    good luck with atom

--- helloworld.erl
+++ helloworld.erl
@@ -3,13 +3,23 @@
 -module(helloworld).
 -export([double/1]).
 -export([hello_world/0]).
+-export([convert/2]).
 
 double(X) ->
   2*X.
 
 hello_world() ->io:fwrite("Good Luck in Erlang").
 
+convert(M, inches) ->
+  M / 2.54;
+
+convert(N,centimeter) ->
+  N * 2.54 .
+  
+%In Erlang Ways Varible must start with Capital Letter'
+%Atom must start with small Letter
 %this simple module is to get multiple value from user not Dobule value funtion"
 %to run this progrem first "erl in terminal"
 %second "c(helloworld) if you change the module name you need to replace yourname with helloworld"
 %third helloword.double(inputvalue)
+%In convert M and N are Variable and inches and centimeter are atom
