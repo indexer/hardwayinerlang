1ae1efd3ea3c49c053d0f00c5fba3fd44e8f1822
Indexer <yemonkyaw007@gmail.com>
2014-06-25 03:50:57 -0400

    hope this way help me to imporve

--- area.erl
+++ area.erl
@@ -2,8 +2,13 @@
 -export([calculate_area/1]).
 
 %this sample is aim to calculate the area of user
-%In This program you must notic you can reuse Variable 
+%In This program you must notic you can reuse Variable
+%between { and } are call turple don't forget it indexer
 calculate_area({square, Side}) ->
   Side * Side;
 calculate_area({rectangle, Length, Width}) ->
-  Length * Width.
+  Length * Width;
+calculate_area({circle, Radius}) ->
+  3.14159 * Radius * Radius;
+calculate_area({triangle, Base, Height}) ->
+  Base * Height / 2.
