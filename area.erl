8fca88a4376dfe6ff6b7f210e42032beff728cf2
Indexer <yemonkyaw007@gmail.com>
2014-06-26 01:12:08 -0400

    note about tuple and List

--- area.erl
+++ area.erl
@@ -12,3 +12,14 @@ calculate_area({circle, Radius}) ->
   3.14159 * Radius * Radius;
 calculate_area({triangle, Base, Height}) ->
   Base * Height / 2.
+
+%Erlang suppot two data type 
+%1. Constant Data Type -> Numbers & Atoms
+%eg . 1 3 4 565 -232323 0.34343
+%eg . abc 'this_is_an_atom'
+
+%2. Compunds Data Type ->Turple and List
+%eg. {circle, 3,3} %tuple start with { and close with }
+%turple use for sorting of fix number item written in squence
+%eg.[abc,{circle,3,3}] %List start with [ and close with ]
+%List are use for used for sorting a variable number of items
